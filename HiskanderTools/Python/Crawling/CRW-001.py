261730effbd9af4882ab65135542b90a3b5a6156
Hskndr <43235508+Hskndr@users.noreply.github.com>
2023-09-20 19:41:04 -0300

    Recursive
    Modificada la función crawl_website para que se ejecute de manera recursiva y así controlar los niveles de profundidad de las páginas hijas.

--- HiskanderTools/Python/Crawling/CRW-001.py
+++ HiskanderTools/Python/Crawling/CRW-001.py
@@ -9,7 +9,8 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 import tldextract
 
-def find_links_on_page(url, target_domain, search_string, current_depth, max_depth):
+# Módodulo para encontrar enlaces en sitios y cadena de caracteres
+def find_links_on_page(url, target_domain, search_strings, current_depth, max_depth):
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -26,50 +27,61 @@ def find_links_on_page(url, target_domain, search_string, current_depth, max_dep
                 if target_domain in parsed_url.netloc:
                     matching_links.append(absolute_url)
 
-        if search_string.lower() in page_text.lower():
-            print(f'Se encontró "{search_string}" en {url}')
+        for search_string in search_strings:
+            if search_string.lower() in page_text.lower():
+                print(f'Se encontró "{search_string}" en {url}')
         
         return matching_links, len(matching_links)
     except Exception as e:
         print(f"Error al acceder a {url}: {e}")
         return [], 0
+## Módodulo para encontrar enlaces en sitios y cadena de caracteres
 
 
-def crawl_website(start_url, target_domain, search_string, max_depth):
+## Módulo para Rastrear Sitio y encontrar enlaces.
+def crawl_website_recursive(url, target_domain, search_strings, max_depth, current_depth=1):
+    if current_depth > max_depth:
+        return []
+
     visited_urls = set()
-    to_visit = [(start_url, 0)]  # Usar una tupla para almacenar la URL y su profundidad
+    to_visit = [(url, current_depth)]  # Usar una tupla para almacenar la URL y su profundidad
     matching_links = []
-    request_counter = 0  # Inicializar el contador de solicitudes
 
     while to_visit:
         url, current_depth = to_visit.pop()
         visited_urls.add(url)
 
-        links_on_page, num_links = find_links_on_page(url, target_domain, search_string, current_depth, max_depth)
+        links_on_page, num_links = find_links_on_page(url, target_domain, search_strings, current_depth, max_depth)
         matching_links.extend(links_on_page)
 
-        print(f"Número de enlaces en {url}: {num_links}, requests: {request_counter}")
+        print(f"Nivel de profundidad {current_depth}: Número de enlaces en {url}: {num_links}")
 
         for link in links_on_page:
             if link not in visited_urls and current_depth < max_depth:
                 to_visit.append((link, current_depth + 1))  # Incrementar la profundidad
 
-        request_counter += 1  # Incrementar el contador de solicitudes
-        
     return matching_links
+## Módulo para Rastrear Sitio y encontrar enlaces.
 
 # Programa Principal
 if __name__ == "__main__":
     start_url = "https://www.eada.edu"  # Cambia esto al sitio web que desees rastrear
     extracted = tldextract.extract(start_url)
     target_domain = f"{extracted.domain}.{extracted.suffix}"
-    search_string = "CIF"  # Cambia esto al string que desees buscar
+    
+    # Lista de cadenas de búsqueda
+    search_strings = [
+        "CIF",
+        "C.I.F.",
+        "Código de Identificación Fiscal"
+        ]  
+    
     max_depth = 5  # Cambia esto al nivel máximo de profundidad que desees
     
     # Print Operativo
     print('Inicio a:',target_domain)
     
-    matching_links = crawl_website(start_url, target_domain, search_string, max_depth)
+    matching_links = crawl_website_recursive(start_url, target_domain, search_strings, max_depth)
 
     # Print Operativo
     print('matching links Culminado:')
